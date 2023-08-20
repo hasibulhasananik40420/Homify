62bc3c176d1574495ac8d6d29613461dad2e368e
hasibulhasananik40420 <hasibulhasananik40420@gmail.com>
2023-08-20 13:49:24 +0600

    blog details page done

--- src/Pages/BlogsDetails/BlogsDetails.jsx
+++ src/Pages/BlogsDetails/BlogsDetails.jsx
@@ -4,10 +4,10 @@ import React from "react";
 import CommonAction from "../../components/CommonAction/CommonAction";
 import blogDetails from "../../assets/Blog-details-image/blog-details.png";
 import blog2 from "../../assets/Blog-details-image/blog-2.png";
+import blogMan from "../../assets/Blog-details-image/blogman-2.png";
+import playImage from "../../assets/Blog-details-image/play-image.png";
 import { GoDotFill } from "react-icons/go";
-import { motion } from "framer-motion";
-
-
+import BlogRelatedCard from "../../components/BlogRelatedCard/BlogRelatedCard";
 
 const BlogsDetails = () => {
   return (
@@ -17,7 +17,8 @@ const BlogsDetails = () => {
       <CommonAction title={"Blog Details "} subTitle={"Home> Blog Details "} />
 
       {/******************* blog details part start here *******************/}
-      <div className="max-w-container mx-auto lg:px-8 2xl:px-0 px-4 md:px-4 2xl:pt-32 lg:pt-[100px] md:pt-[80px] pt-16">
+      {/*------------------------------------------------------------ */}
+      <div className="max-w-container mx-auto lg:px-8 2xl:px-0 px-4 md:px-8 2xl:pt-32 lg:pt-[100px] md:pt-[80px] pt-16">
         <h1 className="text-[#504592] 2xl:text-[56px] lg:text-[56px] md:text-[56px]  text-[32px] font-Teko font-bold 2xl:leading-[56px] lg:leading-[56px]">
           Every Real Estate Agent Needs a Website
         </h1>
@@ -28,7 +29,11 @@ const BlogsDetails = () => {
 
         <div className=" 2xl:flex 2xl:gap-[122px] lg:flex lg:gap-20 mt-20">
           <div className="2xl:w-[950px] lg:w-[950px] w-full h-full">
-            <img className="2xl:w-[950px] lg:w-[950px] 2xl:h-[500px] lg:h-[500px] md:h-[500px] h-[400px]" src={blogDetails} alt="" />
+            <img
+              className="2xl:w-[950px] lg:w-[950px] 2xl:h-[500px] lg:h-[500px] md:h-[500px] h-[400px]"
+              src={blogDetails}
+              alt=""
+            />
             <p className="text-black text-[16px] font-Lato font-normal leading-8 mt-10">
               No matter their age, demographic, or experience with
               buying/selling homes, most clients start their search for a real
@@ -85,9 +90,9 @@ const BlogsDetails = () => {
 
                 <p className="text-black text-[16px] font-Lato font-normal leading-8">
                   Leverage the for homes in your market areas to find your name
-                  first. Leverage the for homes in your market areas to find your name
-                  first. Leverage the for homes in your market areas to find your name
-                  first.
+                  first. Leverage the for homes in your market areas to find
+                  your name first. Leverage the for homes in your market areas
+                  to find your name first.
                 </p>
               </span>
 
@@ -95,7 +100,7 @@ const BlogsDetails = () => {
                 <GoDotFill className="text-[30px]" />
 
                 <p className="text-black text-[16px] font-Lato font-normal leading-8">
-                    Learn more about your brand, discover new neighborhoods, fall
+                  Learn more about your brand, discover new neighborhoods, fall
                   in love with properties in your markets, and contact you
                   directly to take their next steps. A Constant Source of
                   Qualified LeadsNo matter where.
@@ -125,10 +130,8 @@ const BlogsDetails = () => {
               </span>
             </div>
 
-
-
-  <h2 className="text-[#504592] text-[32px] font-Teko font-bold mt-10">
-  How To Get A Great Real Estate Website
+            <h2 className="text-[#504592] text-[32px] font-Teko font-bold mt-10">
+              How To Get A Great Real Estate Website
             </h2>
 
             <div className="flex flex-col gap-2 ml-4 mt-4">
@@ -170,65 +173,135 @@ const BlogsDetails = () => {
 
                 <p className="text-black text-[16px] font-Lato font-normal leading-8">
                   Leverage the for homes in your market areas to find your name
-                  first. Leverage the for homes in your market areas to find your name
-                  first. Leverage the for homes in your market areas to find your name
-                  first.
+                  first. Leverage the for homes in your market areas to find
+                  your name first. Leverage the for homes in your market areas
+                  to find your name first.
                 </p>
               </span>
-
-            
             </div>
 
-             <p className="text-black text-[16px] font-Lato font-normal leading-8 mt-8">Are you ready to create a real estate website that takes your brand to the next level with an innovative team that knows how to reach your target audience? Contact us to learn more about website design, and all of the ways we can help grow your business online.</p>
-
-
-
-
-
-
-
+            <p className="text-black text-[16px] font-Lato font-normal leading-8 mt-8">
+              Are you ready to create a real estate website that takes your
+              brand to the next level with an innovative team that knows how to
+              reach your target audience? Contact us to learn more about website
+              design, and all of the ways we can help grow your business online.
+            </p>
           </div>
 
           <div className="2xl:w-[368px] lg:w-[368px] md:w-[550px] md:mx-auto md:mt-24 lg:mt-0 2xl:mt-0 mt-16 w-full flex flex-col 2xl:gap-y-32 lg:gap-y-[80px]">
-         
-         
-             <div className="relative 2xl:w-[368px] lg:w-[368px] md:w-[550px] w-full">
-             <img className="2xl:w-[368px] lg:w-[368px] md:w-[550px] w-full h-[500px]" src={blog2} alt="" />
+            <div className="relative 2xl:w-[368px] lg:w-[368px] md:w-[550px] w-full">
+              <img
+                className="2xl:w-[368px] lg:w-[368px] md:w-[550px] w-full h-[500px]"
+                src={blog2}
+                alt=""
+              />
               <div className="bg-[#504592] h-[148px] pl-8 pt-8 absolute bottom-0 ">
-                  <h1 className="text-white text-[20px] md:text-[32px] lg:text-[20px] 2xl:text-[20px] font-Lato font-bold leading-10 2xl:leading-10 lg:leading-10 md:leading-[46px]">How To Get A Great Real Estate Website</h1>
+                <h1 className="text-white text-[20px] md:text-[32px] lg:text-[20px] 2xl:text-[20px] font-Lato font-bold leading-10 2xl:leading-10 lg:leading-10 md:leading-[46px]">
+                  How To Get A Great Real Estate Website
+                </h1>
               </div>
-             </div>
-           
-                <div className="relative group 2xl:w-[368px] lg:w-[368px] md:w-[550px] w-full mt-16 md:mt-24 lg:mt-0 2xl:mt-0">
-             <img className="2xl:w-[368px] lg:w-[368px] md:w-[550px] w-full h-[500px]" src={blog2} alt="" />
+            </div>
+
+            <div className="relative group 2xl:w-[368px] lg:w-[368px] md:w-[550px] w-full mt-16 md:mt-24 lg:mt-0 2xl:mt-0">
+              <img
+                className="2xl:w-[368px] lg:w-[368px] md:w-[550px] w-full h-[500px]"
+                src={blog2}
+                alt=""
+              />
               <div className="bg-[#504592] 2xl:w-[368px] lg:w-[368px] md:w-[550px] w-full h-[148px] pl-8 pt-8 absolute bottom-0 transform translate-y-full transition-transform duration-1000 group-hover:translate-y-0 hidden group-hover:block">
-                  <h1 className="text-white text-[20px] md:text-[32px] lg:text-[20px] 2xl:text-[20px] font-Lato font-bold leading-10 2xl:leading-10 lg:leading-10 md:leading-[46px]">How To Get A Great Real Estate Website</h1>
+                <h1 className="text-white text-[20px] md:text-[32px] lg:text-[20px] 2xl:text-[20px] font-Lato font-bold leading-10 2xl:leading-10 lg:leading-10 md:leading-[46px]">
+                  How To Get A Great Real Estate Website
+                </h1>
               </div>
-             </div>
-           
-                <div className="relative group 2xl:w-[368px] lg:w-[368px] md:w-[550px] w-full mt-16 md:mt-24 lg:mt-0 2xl:mt-0">
-             <img className="2xl:w-[368px] lg:w-[368px] md:w-[550px] w-full h-[500px]" src={blog2} alt="" />
+            </div>
+
+            <div className="relative group 2xl:w-[368px] lg:w-[368px] md:w-[550px] w-full mt-16 md:mt-24 lg:mt-0 2xl:mt-0">
+              <img
+                className="2xl:w-[368px] lg:w-[368px] md:w-[550px] w-full h-[500px]"
+                src={blog2}
+                alt=""
+              />
               <div className="bg-[#504592] 2xl:w-[368px] lg:w-[368px] md:w-[550px] w-full h-[148px] pl-8 pt-8 absolute bottom-0 transform translate-y-full transition-transform duration-1000 group-hover:translate-y-0 hidden group-hover:block">
-                  <h1 className="text-white text-[20px] md:text-[32px] lg:text-[20px] 2xl:text-[20px] font-Lato font-bold leading-10 2xl:leading-10 lg:leading-10 md:leading-[46px]">How To Get A Great Real Estate Website</h1>
+                <h1 className="text-white text-[20px] md:text-[32px] lg:text-[20px] 2xl:text-[20px] font-Lato font-bold leading-10 2xl:leading-10 lg:leading-10 md:leading-[46px]">
+                  How To Get A Great Real Estate Website
+                </h1>
               </div>
-             </div>
-
-
-
-
-
-
-
-
-
-           
-            
-           
+            </div>
           </div>
         </div>
+
+        {/******************* blog hero part start here *******************/}
+        {/*------------------------------------------------------------ */}
+        <div className="lg:flex lg:justify-center 2xl:pt-32 lg:pt-[100px] md:pt-[80px] pt-16">
+          <div className="2xl:w-[720px] 2xl:h-[600px] lg:w-full lg:h-[550px] w-full h-[550px] md:h-[600px] bg-[#181C21] 2xl:py-[118px] 2xl:px-[110px] hover:bg-secondaryColor duration-500 hover:cursor-pointer">
+            <div className="flex gap-5 items-center lg:pl-10 2xl:pl-0 lg:pt-20 2xl:pt-0 pl-5 md:pl-10 pt-16 md:pt-20">
+              <span>
+                <img
+                  className="w-[100px] h-[100px] rounded-[8px]"
+                  src={blogMan}
+                  alt=""
+                />
+              </span>
+
+              <span>
+                <h1 className="text-white text-[32px] font-Teko font-bold tracking-[3.2px]">
+                  John Martin
+                </h1>
+                <p className="text-white lg:text-[20px] 2xl:text-[20px] md:text-[20px] text-[12px] font-Lato font-light">
+                  Real Estate Expert Agent
+                </p>
+              </span>
+            </div>
+
+            <h1 className="text-white lg:text-[56px] 2xl:text-[56px] md:text-[56px] text-[32px] font-Lato font-bold mt-1 lg:pl-10 2xl:pl-0 md:pl-10 pl-5 pt-6 md:pt-10 lg:pt-0 2xl:pt-0">
+              How To Get A Great Real Estate House.
+            </h1>
+            <p className="mt-5 text-white text-[16px] md:text-[20px] lg:text-[16px] 2xl:text-[16px] font-Lato font-normal leading-8 2xl:w-[536px] lg:pl-10 2xl:pl-0 pl-5 md:pl-10">
+              Are you ready to create a real estate website that takes your
+              brand to the next level with an innovative team that knows how to
+              reach that an your target audience?
+              <span className="text-white text-[16px] font-Lato font-normal leading-8 underline hover:text-secondaryColor hover:cursor-pointer duration-500">
+                {" "}
+                Contact us
+              </span>{" "}
+              to learn team level that .
+            </p>
+          </div>
+
+          <div
+            className="2xl:w-[720px] 2xl:h-[600px] lg:w-full lg:h-[550px] md:h-[600px] h-[550px] "
+            style={{ background: "rgba(62, 52, 143, 0.60)" }}
+          >
+            <img
+              className="2xl:w-[720px] 2xl:h-[600px] lg:w-full lg:h-[550px] md:h-[600px] h-[550px] opacity-40 hover:opacity-70 hover:cursor-pointer duration-500"
+              src={playImage}
+              alt=""
+            />
+          </div>
+        </div>
+
+        {/******************* blog hero part end here *******************/}
+        {/*------------------------------------------------------------ */}
+
+        {/******************* blog releted post card start here *******************/}
+        {/*---------------------------------------------------------------------- */}
+
+        <div className="2xl:pt-32 lg:pt-[100px] md:pt-[80px] pt-16">
+          <h1 className="text-[#3E348F] text-[56px] font-Teko font-bold leading-[70px]">
+            See Related Post
+          </h1>
+
+           <div>
+             <BlogRelatedCard/>
+           </div>
+        </div>
+
+        {/******************* blog releted post card end here *******************/}
+        {/*---------------------------------------------------------------------- */}
       </div>
 
       {/******************* blog details part end here *******************/}
+      {/*------------------------------------------------------------ */}
     </div>
 
     //********************  Blog details section start here **************************//
---
+++ src/components/BlogRelatedCard/BlogRelatedCard.jsx
@@ -0,0 +1,234 @@
+import { FiUser } from "react-icons/fi";
+
+const BlogRelatedCard = () => {
+  return (
+    <div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-3  gap-x-[20px] gap-y-[50px] justify-items-center mt-10">
+      
+      
+        {/******************* blog releted card 1 start here *******************/}
+        <div className="2xl:w-[466px] lg:w-[400px] w-full">
+          <img
+            className="2xl:w-[466px] lg:w-[400px] w-full h-[400px] border"
+            src="https://img.freepik.com/premium-photo/house-is-listed-1-95-million_865967-353542.jpg?size=626&ext=jpg&ga=GA1.2.1166836568.1647150580&semt=ais"
+            style={{
+              borderRadius: "60px 60px 60px 0px",
+              boxShadow: "0px 4px 60px 0px rgba(221, 219, 219, 0.30)",
+            }}
+            alt=""
+          />
+
+          <div>
+            <h5 className="text-[#3E348F] text-[16px] font-Lato font-bold mt-10">
+              Last updated June 9, 2023
+            </h5>
+
+            <h1 className="text-[#263238] text-[32px] font-Teko font-bold 2xl:w-[404px] w-full mt-1">
+              4 Ways to Boost Your Marketing in a Down Market
+            </h1>
+
+            <p className="text-[#3E348F] text-[20px] font-Lato font-bold mt-2">
+              Continue Reading
+            </p>
+
+            <span className="flex items-center gap-3 mt-2">
+              <FiUser className="text-[30px]" />
+              <p className="text-[#545759] text-[16px] font-Lato font-normal">
+                By Water Loo Smith
+              </p>
+            </span>
+          </div>
+        </div>
+        {/******************* blog releted card 1 end here *******************/}
+     
+       {/******************* blog releted card 1 start here *******************/}
+        <div className="2xl:w-[466px] lg:w-[400px] w-full">
+          <img
+            className="2xl:w-[466px] lg:w-[400px] w-full h-[400px] border"
+            src="https://img.freepik.com/premium-photo/house-is-listed-1-95-million_865967-353542.jpg?size=626&ext=jpg&ga=GA1.2.1166836568.1647150580&semt=ais"
+            style={{
+              borderRadius: "60px 60px 60px 0px",
+              boxShadow: "0px 4px 60px 0px rgba(221, 219, 219, 0.30)",
+            }}
+            alt=""
+          />
+
+          <div>
+            <h5 className="text-[#3E348F] text-[16px] font-Lato font-bold mt-10">
+              Last updated June 9, 2023
+            </h5>
+
+            <h1 className="text-[#263238] text-[32px] font-Teko font-bold 2xl:w-[404px] w-full mt-1">
+              4 Ways to Boost Your Marketing in a Down Market
+            </h1>
+
+            <p className="text-[#3E348F] text-[20px] font-Lato font-bold mt-2">
+              Continue Reading
+            </p>
+
+            <span className="flex items-center gap-3 mt-2">
+              <FiUser className="text-[30px]" />
+              <p className="text-[#545759] text-[16px] font-Lato font-normal">
+                By Water Loo Smith
+              </p>
+            </span>
+          </div>
+        </div>
+        {/******************* blog releted card 1 end here *******************/}
+     
+       {/******************* blog releted card 1 start here *******************/}
+        <div className="2xl:w-[466px] lg:w-[400px] w-full">
+          <img
+            className="2xl:w-[466px] lg:w-[400px] w-full h-[400px] border"
+            src="https://img.freepik.com/premium-photo/house-is-listed-1-95-million_865967-353542.jpg?size=626&ext=jpg&ga=GA1.2.1166836568.1647150580&semt=ais"
+            style={{
+              borderRadius: "60px 60px 60px 0px",
+              boxShadow: "0px 4px 60px 0px rgba(221, 219, 219, 0.30)",
+            }}
+            alt=""
+          />
+
+          <div>
+            <h5 className="text-[#3E348F] text-[16px] font-Lato font-bold mt-10">
+              Last updated June 9, 2023
+            </h5>
+
+            <h1 className="text-[#263238] text-[32px] font-Teko font-bold 2xl:w-[404px] w-full mt-1">
+              4 Ways to Boost Your Marketing in a Down Market
+            </h1>
+
+            <p className="text-[#3E348F] text-[20px] font-Lato font-bold mt-2">
+              Continue Reading
+            </p>
+
+            <span className="flex items-center gap-3 mt-2">
+              <FiUser className="text-[30px]" />
+              <p className="text-[#545759] text-[16px] font-Lato font-normal">
+                By Water Loo Smith
+              </p>
+            </span>
+          </div>
+        </div>
+        {/******************* blog releted card 1 end here *******************/}
+     
+       {/******************* blog releted card 1 start here *******************/}
+        <div className="2xl:w-[466px] lg:w-[400px] w-full">
+          <img
+            className="2xl:w-[466px] lg:w-[400px] w-full h-[400px] border"
+            src="https://img.freepik.com/premium-photo/house-is-listed-1-95-million_865967-353542.jpg?size=626&ext=jpg&ga=GA1.2.1166836568.1647150580&semt=ais"
+            style={{
+              borderRadius: "60px 60px 60px 0px",
+              boxShadow: "0px 4px 60px 0px rgba(221, 219, 219, 0.30)",
+            }}
+            alt=""
+          />
+
+          <div>
+            <h5 className="text-[#3E348F] text-[16px] font-Lato font-bold mt-10">
+              Last updated June 9, 2023
+            </h5>
+
+            <h1 className="text-[#263238] text-[32px] font-Teko font-bold 2xl:w-[404px] w-full mt-1">
+              4 Ways to Boost Your Marketing in a Down Market
+            </h1>
+
+            <p className="text-[#3E348F] text-[20px] font-Lato font-bold mt-2">
+              Continue Reading
+            </p>
+
+            <span className="flex items-center gap-3 mt-2">
+              <FiUser className="text-[30px]" />
+              <p className="text-[#545759] text-[16px] font-Lato font-normal">
+                By Water Loo Smith
+              </p>
+            </span>
+          </div>
+        </div>
+        {/******************* blog releted card 1 end here *******************/}
+     
+       {/******************* blog releted card 1 start here *******************/}
+        <div className="2xl:w-[466px] lg:w-[400px] w-full">
+          <img
+            className="2xl:w-[466px] lg:w-[400px] w-full h-[400px] border"
+            src="https://img.freepik.com/premium-photo/house-is-listed-1-95-million_865967-353542.jpg?size=626&ext=jpg&ga=GA1.2.1166836568.1647150580&semt=ais"
+            style={{
+              borderRadius: "60px 60px 60px 0px",
+              boxShadow: "0px 4px 60px 0px rgba(221, 219, 219, 0.30)",
+            }}
+            alt=""
+          />
+
+          <div>
+            <h5 className="text-[#3E348F] text-[16px] font-Lato font-bold mt-10">
+              Last updated June 9, 2023
+            </h5>
+
+            <h1 className="text-[#263238] text-[32px] font-Teko font-bold 2xl:w-[404px] w-full mt-1">
+              4 Ways to Boost Your Marketing in a Down Market
+            </h1>
+
+            <p className="text-[#3E348F] text-[20px] font-Lato font-bold mt-2">
+              Continue Reading
+            </p>
+
+            <span className="flex items-center gap-3 mt-2">
+              <FiUser className="text-[30px]" />
+              <p className="text-[#545759] text-[16px] font-Lato font-normal">
+                By Water Loo Smith
+              </p>
+            </span>
+          </div>
+        </div>
+        {/******************* blog releted card 1 end here *******************/}
+     
+       {/******************* blog releted card 1 start here *******************/}
+        <div className="2xl:w-[466px] lg:w-[400px] w-full">
+          <img
+            className="2xl:w-[466px] lg:w-[400px] w-full h-[400px] border"
+            src="https://img.freepik.com/premium-photo/house-is-listed-1-95-million_865967-353542.jpg?size=626&ext=jpg&ga=GA1.2.1166836568.1647150580&semt=ais"
+            style={{
+              borderRadius: "60px 60px 60px 0px",
+              boxShadow: "0px 4px 60px 0px rgba(221, 219, 219, 0.30)",
+            }}
+            alt=""
+          />
+
+          <div>
+            <h5 className="text-[#3E348F] text-[16px] font-Lato font-bold mt-10">
+              Last updated June 9, 2023
+            </h5>
+
+            <h1 className="text-[#263238] text-[32px] font-Teko font-bold 2xl:w-[404px] w-full mt-1">
+              4 Ways to Boost Your Marketing in a Down Market
+            </h1>
+
+            <p className="text-[#3E348F] text-[20px] font-Lato font-bold mt-2">
+              Continue Reading
+            </p>
+
+            <span className="flex items-center gap-3 mt-2">
+              <FiUser className="text-[30px]" />
+              <p className="text-[#545759] text-[16px] font-Lato font-normal">
+                By Water Loo Smith
+              </p>
+            </span>
+          </div>
+        </div>
+        {/******************* blog releted card 1 end here *******************/}
+     
+     
+      </div>
+
+     <div className="flex justify-center mt-14 2xl:mb-32 lg:mb-[100px] md:mb-[80px] mb-[64px]">
+     <button className="bg-[#F90] text-white text-[16px] font-Lato font-normal w-[170px] h-[56px] custom-border-property  hover:bg-primary transform ease-in-out duration-500 ">
+      Load More
+    </button>
+     </div>
+
+
+
+    </div>
+  );
+};
+
+export default BlogRelatedCard;
