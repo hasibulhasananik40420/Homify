00bf6c2c026c5a5cbb52475d48f9e01cbef80657
hasibulhasananik40420 <hasibulhasananik40420@gmail.com>
2023-10-10 19:21:41 +0600

    remove framer-motion

--- src/Pages/ContactUsPage/Faq/FaqSection .jsx
+++ src/Pages/ContactUsPage/Faq/FaqSection .jsx
@@ -14,17 +14,10 @@ const FaqSection = ({ faqData }) => {
     <div className="">
       {faqData.map((faq, index) => (
         <div key={faq._id} className="mb-5">
-          <motion.div
+          <div
             className="bg-[#3E348F] lg:h-[60px] md:h-[60px] h-auto py-1 md:py-0 lg:py-0 2xl:py-0 flex justify-between items-center cursor-pointer px-8"
             onClick={() => toggleQuestion(index)}
-            initial={{ backgroundColor: '#3E348F' }}
-            animate={{
-              backgroundColor: activeIndex === index ? '#F90' : '#3E348F',
-              borderBottomLeftRadius: activeIndex === index ? 0 : '15px',
-              borderBottomRightRadius: activeIndex === index ? 0 : '15px',
-            }}
-            whileHover={{ backgroundColor: '#5650A2' }}
-            transition={{ duration: 0.3, ease: 'easeInOut' }}
+           
             style={{
               borderTop: '1px solid #3E348F',
               borderLeft: '1px solid #3E348F',
@@ -38,32 +31,27 @@ const FaqSection = ({ faqData }) => {
                 activeIndex === index ? 'transform rotate-45' : ''
               }`}
             />
-          </motion.div>
-          <AnimatePresence>
+          </div>
+         
             {activeIndex === index && (
-              <motion.div
+              <div
                 key="answer"
-                initial={{ opacity: 0, height: 0 }}
-                animate={{ opacity: 1, height: 'auto' }}
-                exit={{ opacity: 0, height: 0 }}
-                transition={{ duration: 0.3 }}
+                
                 style={{
                   borderTop: '1px solid #878383',
                   borderBottom: '1px solid #878383',
                   background: '#FFF',
                 }}
               >
-                <motion.p
-                  initial={{ opacity: 0 }}
-                  animate={{ opacity: 1 }}
-                  transition={{ duration: 0.3, delay: 0.3 }}
+                <p
+                 
                   className="mt-2 text-[16px] text-[#141414] font-Lato font-normal leading-8 px-8"
                 >
                   {faq.answar}
-                </motion.p>
-              </motion.div>
+                </p>
+              </div>
             )}
-          </AnimatePresence>
+        
         </div>
       ))}
     </div>
